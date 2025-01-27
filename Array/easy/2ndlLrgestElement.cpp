8d585f72734bf169c100d4397d2f085a9b01d4ba
Ashraf Khan Shihab <141819910+0-shihab-0@users.noreply.github.com>
2025-01-27 21:52:00 +0600

    Create 2ndlLrgestElement.cpp

---
+++ Array/easy/2ndlLrgestElement.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    vector<int>arr;
+
+    for(int i=0;i<arr.size();i++){
+        cin>>arr[i];
+    }
+  
+
+    for(int out:arr){
+        cout<<out;
+    }
+
+   return 0;
+}
+
